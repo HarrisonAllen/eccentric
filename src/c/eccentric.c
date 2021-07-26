f7b1eb10a24a87e5e114c241cbee500e8117d107
Harrison Allen <hmallen@mit.edu>
2021-07-26 03:39:44 -0400

    added clay, seconds, battery indicator, more colors!

--- src/c/eccentric.c
+++ src/c/eccentric.c
@@ -1,10 +1,22 @@
 #include <pebble.h>
 
+#define CLAY_SAVE_KEY 1
+
+typedef struct ClaySettings {
+  GColor BackgroundOuterColor;
+  GColor BackgroundInnerColor;
+  GColor HourColor;
+  GColor MinuteColor;
+  bool SecondsEnabled;
+  GColor SecondsColor;
+  bool BatteryEnabled;
+  GColor BatteryColor;
+} ClaySettings;
+
 static Window *s_window;
 static Layer *s_canvas_layer;
-static uint32_t s_bg_color = 0xffffff;
-static uint32_t s_hour_color = 0x000000;
-static uint32_t s_min_color = 0xffffff;
+
+ClaySettings settings;
 
 static int clamp(int val, int min_val, int max_val) {
   if (val < min_val) {
@@ -22,18 +34,23 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   struct tm *cur_time = localtime(&unadjusted_time);
   uint8_t hour = cur_time->tm_hour % 12;
   uint8_t min = cur_time->tm_min;
+  uint8_t sec = cur_time->tm_sec;
 
   GPoint center = GPoint(layer_bounds.size.w / 2, layer_bounds.size.h / 2);
   graphics_context_set_stroke_width(ctx, 0);
 #if defined(PBL_RECT)
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, layer_bounds, 0, GCornerNone);
-  graphics_context_set_fill_color(ctx, GColorFromHEX(s_bg_color));
-  graphics_fill_circle(ctx, center, layer_bounds.size.w / 2 - 1);
-#else
-  graphics_context_set_fill_color(ctx, GColorFromHEX(s_bg_color));
+  graphics_context_set_fill_color(ctx, settings.BackgroundOuterColor);
   graphics_fill_rect(ctx, layer_bounds, 0, GCornerNone);
 #endif
+  graphics_context_set_fill_color(ctx, settings.BackgroundInnerColor);
+  graphics_fill_circle(ctx, center, layer_bounds.size.w / 2 - 1);
+
+  if (settings.BatteryEnabled) {
+    BatteryChargeState battery_state = battery_state_service_peek();
+
+    graphics_context_set_fill_color(ctx, settings.BatteryColor);
+    graphics_fill_circle(ctx, center, (layer_bounds.size.w / 2 - 1) * battery_state.charge_percent / 100);
+  }
 
   int32_t hour_angle = TRIG_MAX_ANGLE * hour / 12 + TRIG_MAX_ANGLE / 12 * min / 60;
   int32_t hour_hand_length = PBL_IF_ROUND_ELSE(layer_bounds.size.w / 4, layer_bounds.size.h / 4);
@@ -50,7 +67,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   hour_hand.x = clamp(hour_hand.x, min_x, max_x);
 #endif
   uint16_t hour_radius = hour_hand_length * 2 / 3;
-  graphics_context_set_fill_color(ctx, GColorFromHEX(s_hour_color));
+  graphics_context_set_fill_color(ctx, settings.HourColor);
   graphics_fill_circle(ctx, hour_hand, hour_radius);
   
   int32_t min_angle = TRIG_MAX_ANGLE * min / 60;
@@ -60,8 +77,20 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   min_hand.y = (-cos_lookup(min_angle) * min_hand_length / TRIG_MAX_RATIO) + min_center.y;
   min_hand.x = (sin_lookup(min_angle) * min_hand_length / TRIG_MAX_RATIO) + min_center.x;
   uint16_t min_radius = hour_radius / 3;
-  graphics_context_set_fill_color(ctx, GColorFromHEX(s_min_color));
+  graphics_context_set_fill_color(ctx, settings.MinuteColor);
   graphics_fill_circle(ctx, min_hand, min_radius);
+  
+  if (settings.SecondsEnabled) {
+    int32_t sec_angle = TRIG_MAX_ANGLE * sec / 60;
+    int32_t sec_hand_length = min_hand_length / 3;
+    GPoint sec_center = min_hand;
+    GPoint sec_hand;
+    sec_hand.y = (-cos_lookup(sec_angle) * sec_hand_length / TRIG_MAX_RATIO) + sec_center.y;
+    sec_hand.x = (sin_lookup(sec_angle) * sec_hand_length / TRIG_MAX_RATIO) + sec_center.x;
+    uint16_t sec_radius = min_radius / 3;
+    graphics_context_set_fill_color(ctx, settings.SecondsColor);
+    graphics_fill_circle(ctx, sec_hand, sec_radius);
+  }
 }
 
 static void time_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -83,9 +112,92 @@ static void window_unload(Window *window) {
   layer_destroy(s_canvas_layer);
 }
 
+static void default_settings() {
+  settings.BackgroundOuterColor = GColorWhite;
+  settings.BackgroundInnerColor = GColorWhite;
+  settings.HourColor = GColorBlack;
+  settings.MinuteColor = GColorWhite;
+  settings.SecondsEnabled = false;
+  settings.SecondsColor = GColorBlack;
+  settings.BatteryEnabled = false;
+  settings.BatteryColor = GColorLightGray;
+}
+
+static void load_settings() {
+  default_settings();
+
+  persist_read_data(CLAY_SAVE_KEY, &settings, sizeof(settings));
+}
+
+static void save_settings() {
+  persist_write_data(CLAY_SAVE_KEY, &settings, sizeof(settings));
+
+  layer_mark_dirty(s_canvas_layer);
+}
+
+static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  // Read color preferences
+  Tuple *bg_outer_color_t = dict_find(iter, MESSAGE_KEY_BackgroundOuterColor);
+  if(bg_outer_color_t) {
+    settings.BackgroundOuterColor = GColorFromHEX(bg_outer_color_t->value->int32);
+  }
+  
+  Tuple *bg_inner_color_t = dict_find(iter, MESSAGE_KEY_BackgroundInnerColor);
+  if(bg_inner_color_t) {
+    settings.BackgroundInnerColor = GColorFromHEX(bg_inner_color_t->value->int32);
+  }
+  
+  Tuple *hour_color_t = dict_find(iter, MESSAGE_KEY_HourColor);
+  if(hour_color_t) {
+    settings.HourColor = GColorFromHEX(hour_color_t->value->int32);
+  }
+  
+  Tuple *minute_color_t = dict_find(iter, MESSAGE_KEY_MinuteColor);
+  if(minute_color_t) {
+    settings.MinuteColor = GColorFromHEX(minute_color_t->value->int32);
+  }
+
+  Tuple *seconds_enabled_t = dict_find(iter, MESSAGE_KEY_SecondsEnabled);
+  if(seconds_enabled_t) {
+    settings.SecondsEnabled = seconds_enabled_t->value->int32 == 1;
+    tick_timer_service_subscribe(settings.SecondsEnabled ? SECOND_UNIT : MINUTE_UNIT, time_handler);
+  }
+  
+  Tuple *seconds_color_t = dict_find(iter, MESSAGE_KEY_SecondsColor);
+  if(seconds_color_t) {
+    settings.SecondsColor = GColorFromHEX(seconds_color_t->value->int32);
+  }
+
+  Tuple *battery_enabled_t = dict_find(iter, MESSAGE_KEY_BatteryEnabled);
+  if(battery_enabled_t) {
+    settings.BatteryEnabled = battery_enabled_t->value->int32 == 1;
+  }
+  
+  Tuple *battery_color_t = dict_find(iter, MESSAGE_KEY_BatteryColor);
+  if(battery_color_t) {
+    settings.BatteryColor = GColorFromHEX(battery_color_t->value->int32);
+  }
+
+  save_settings();
+}
+
+static void battery_callback(BatteryChargeState state) {
+  layer_mark_dirty(s_canvas_layer);
+}
+
 static void init(void) {
   s_window = window_create();
-  tick_timer_service_subscribe(MINUTE_UNIT, time_handler);
+
+  battery_state_service_subscribe(battery_callback);
+
+  // Load any previous Clay settings
+  load_settings();
+  
+  tick_timer_service_subscribe(settings.SecondsEnabled ? SECOND_UNIT : MINUTE_UNIT, time_handler);
+
+  // Open AppMessage connection
+  app_message_register_inbox_received(inbox_received_handler);
+  app_message_open(128, 128);
 
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = window_load,
